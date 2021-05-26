e52283990ab6fd1096d6dee3afd95bf440c5e71c
Rashid Cruz <cruzrashidl27@gmail.com>
2021-05-26 13:53:54 +0800

    added upperleft button

--- TicTacToe/TicTacToe Game.Designer.cs
+++ TicTacToe/TicTacToe Game.Designer.cs
@@ -28,12 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.upperleft_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // upperleft_btn
+            // 
+            this.upperleft_btn.Location = new System.Drawing.Point(31, 91);
+            this.upperleft_btn.Name = "upperleft_btn";
+            this.upperleft_btn.Size = new System.Drawing.Size(75, 75);
+            this.upperleft_btn.TabIndex = 0;
+            this.upperleft_btn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 373);
+            this.Controls.Add(this.upperleft_btn);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button upperleft_btn;
     }
 }
 
